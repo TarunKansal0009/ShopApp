aedc7c5d22171f97205e96ab0f36143920c471a2
Tarun Kansal <56118843+TarunKansal0009@users.noreply.github.com>
2021-07-29 18:56:13 +0530

    Sign Up

--- shop_app/lib/screens/auth_screen.dart
+++ shop_app/lib/screens/auth_screen.dart
@@ -1,9 +1,7 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'dart:math';
-
-enum AuthMode { Signup, Login }
+import 'package:shop_app/widgets/auth_card.dart';
 
 class AuthScreen extends StatelessWidget {
   static const routeName = '/auth';
@@ -54,7 +52,7 @@ class AuthScreen extends StatelessWidget {
                         ],
                       ),
                       child: Text(
-                        'IET',
+                        'IET Student Chapter',
                         style: TextStyle(
                           color:
                               Theme.of(context).accentTextTheme.bodyText1.color,
@@ -74,157 +72,6 @@ class AuthScreen extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class AuthCard extends StatefulWidget {
-  // const AuthCard({ Key? key }) : super(key: key);
-
-  @override
-  _AuthCardState createState() => _AuthCardState();
-}
-
-class _AuthCardState extends State<AuthCard> {
-  final GlobalKey<FormState> _formkey = GlobalKey();
-  AuthMode _authMode = AuthMode.Login;
-  Map<String, String> _authData = {
-    'email': '',
-    'passward': '',
-  };
-  var _isLoading = false;
-  final _passwardController = TextEditingController();
-  void _submit() {
-    if (!_formkey.currentState.validate()) {
-      return;
-    }
-    _formkey.currentState.save();
-    setState(() {
-      _isLoading = true;
-    });
-    if (_authMode == AuthMode.Login) {
-    } else {}
-    setState(() {
-      _isLoading = false;
-    });
-  }
-
-  void _switchAuthMode() {
-    if (_authMode == AuthMode.Login) {
-      setState(() {
-        _authMode = AuthMode.Signup;
-      });
-    } else {
-      setState(() {
-        _authMode = AuthMode.Login;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final deviceSize = MediaQuery.of(context).size;
-
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      elevation: 8.0,
-      child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 360 : 260),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-            key: _formkey,
-            child: SingleChildScrollView(
-                child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Email'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value.isEmpty || !value.contains('@')) {
-                      return 'Invalid Email';
-                    }
-                  },
-                  onSaved: (value) {
-                    _authData['email'] = value;
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'passward'),
-                  controller: _passwardController,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value.isEmpty || value.length < 5) {
-                      return 'Passward is too short';
-                    }
-                  },
-                  onSaved: (value) {
-                    _authData['password'] = value;
-                  },
-                ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwardController.value) {
-                              return 'passward do not matched';
-                            }
-                          }
-                        : null,
-                  ),
-                SizedBox(
-                  height: 20,
-                ),
-                if (_isLoading)
-                  CircularProgressIndicator()
-                else
-                  ElevatedButton(
-                    onPressed: _submit,
-                    child:
-                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGNUP'),
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      padding: MaterialStateProperty.all(
-                        EdgeInsets.symmetric(horizontal: 30, vertical: 8.0),
-                      ),
-                      textStyle: MaterialStateProperty.all<TextStyle>(
-                        TextStyle(
-                            color: Theme.of(context)
-                                .primaryTextTheme
-                                .button
-                                .color),
-                      ),
-                      overlayColor: MaterialStateProperty.all<Color>(
-                          Theme.of(context).primaryColor),
-                    ),
-                  ),
-                TextButton(
-                  onPressed: _switchAuthMode,
-                  child: Text(
-                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
-                  style: TextButton.styleFrom(
-                    padding: (EdgeInsets.symmetric(
-                      horizontal: 30,
-                      vertical: 4,
-                    )),
-                    textStyle: TextStyle(color: Theme.of(context).primaryColor),
-                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  ),
-                ),
-              ],
-            ))),
       ),
     );
   }
